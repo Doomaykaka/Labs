57c044ff62c4532fc89f884ef3d02bc2b9e72c67
Doomaykaka <73484636+Doomaykaka@users.noreply.github.com>
2021-11-30 13:13:40 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,32 @@
+<!DOCKTYPE html>
+<html>
+ <meta charset="utf-8">
+ <head>
+  <style>
+   body{
+    color:#f8b475;
+    font-family: Comic Sans MS;
+   }
+  </style>
+  <title>
+   Меню
+  </title>
+ </head>
+ <body bgcolor=#000000>
+  <p align="center">
+   <font size="+3">
+    Лабораторные работы по дисциплине Интернет программирование
+    <br><br>
+    Студента группы ИСТбд-32 Егорова Ивана
+    <br><br>
+    <a href="http://ip-labs-istbd-32.atwebpages.com/lab1/lab1.php">Лабораторная работа №1</a>
+    <br><br>
+    <a href="http://ip-labs-istbd-32.atwebpages.com/lab2/lab2.php">Лабораторная работа №2</a>
+    <br><br>
+    <a href="http://ip-labs-istbd-32.atwebpages.com/lab3/lab3.php">Лабораторная работа №3</a>
+    <br><br>
+    <a href="">Лабораторная работа №4</a>
+   </font>
+  </p>
+ </body>
+</html>
